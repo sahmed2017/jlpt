99291fa25d7b478c56f9ccac9d61bc74e7721449
Sara Ahmed <118930981+sahmed2017@users.noreply.github.com>
2023-06-06 00:03:49 -0500

    Update code.py

--- code.py
+++ code.py
@@ -9,38 +9,18 @@ def jlpt_kanji(word):
     print(word)
     print("")
 
-    # Load the N5 vocabulary words from a text file
-    with open("n5_kanji.txt") as f:
-        n5_kanji = [line.strip() for line in f.readlines()]
+    jlpt_levels = {
+        "N5": "n5_kanji.txt",
+        "N4": "n4_kanji.txt",
+        "N3": "n3_kanji.txt",
+        "N2": "n2_kanji.txt",
+        "N1": "n1_kanji.txt"
+    }
 
-    for item in n5_kanji:
-        if item in word:
-            print("N5 Kanji: {} ({})".format(item, conv.do(item)))
+    for level, file_name in jlpt_levels.items():
+        with open(file_name) as f:
+            kanji_list = [line.strip() for line in f.readlines()]
 
-    with open("n4_kanji.txt") as f:
-        n4_kanji = [line.strip() for line in f.readlines()]
-    
-    for item in n4_kanji:
-        if item in word:
-            print("N4 Kanji: {} ({})".format(item, conv.do(item)))
-
-    with open("n3_kanji.txt") as f:
-        n3_kanji = [line.strip() for line in f.readlines()]
-
-    for item in n3_kanji:
-        if item in word:
-            print("N3 Kanji: {} ({})".format(item, conv.do(item)))
-    
-    with open("n2_kanji.txt") as f:
-        n2_kanji = [line.strip() for line in f.readlines()]
-
-    for item in n2_kanji:
-        if item in word:
-            print("N2 Kanji: {} ({})".format(item, conv.do(item)))
-
-    with open("n1_kanji.txt") as f:
-        n1_kanji = [line.strip() for line in f.readlines()]
-
-    for item in n1_kanji:
-        if item in word:
-            print("N1 Kanji: {} ({})".format(item, conv.do(item)))
+        for item in kanji_list:
+            if item in word:
+                print("{} Kanji: {} ({})".format(level, item, conv.do(item)))
